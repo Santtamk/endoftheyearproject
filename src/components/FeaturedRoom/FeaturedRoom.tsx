e5ad140945db10f96ba8dfe1d6207c532c733cc3
Santam Kumai <santtamk@gmail.com>
2024-12-17 18:02:56 +0530

    need to resolve: [ Server ] Error: unexpected token "=", expected expression

--- src/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/components/FeaturedRoom/FeaturedRoom.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+'use client'
 
-const FeaturedRoom = () => {
+import { Room } from "@/app/models/room";
+import Image from "next/image";
+import React, { FC } from "react";
+
+type Props = {
+  featuredRoom: Room;
+};
+
+const FeaturedRoom: FC<Props> = (props) => {
+  const { featuredRoom } = props;
+
   return (
-    <div>FeaturedRoom</div>
-  )
-}
+    <section className="flex md:flex-row flex-col px-4 py-10 items-center gap-12 container">
+      <div className="md:grid gap-8 grid-cols-1">
+        <div className="rounded-2xl overflow-hidden h-48 mb-4 md:mb-0">
+          <Image
+            src={featuredRoom.coverImage.url}
+            alt={featuredRoom.name}
+            width={300}
+            height={300}
+            className="img scale-animation"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default FeaturedRoom
+export default FeaturedRoom;
--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import ThemeContext from "@/context/themeContext";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const themeFromStorage =
+  const themeFromStorage: boolean = 
     typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
       ? JSON.parse(localStorage.getItem("hotel-theme")!)
       : false;
